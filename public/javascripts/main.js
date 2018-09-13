e4215ee30da57d8d9e684528846be0bc4317242a
Tom McCarthy <tpmccarthy1@gmail.com>
2018-09-12 20:46:58 -0400

    Fixed namespacing

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,56 +1,76 @@
-/**
-* AJAX Calls
-**/
 
-function ajaxBtn(){
-	var btn = document.querySelector('#submit-btn');
-	btn.addEventListener('click', submit);
-}
+//Self invoking function
 
+(function () { 
+
+//Submit form function
 function submit(){
-	var url = '/result';
+  var url = '/result';
+  var station = document.querySelector('#station').value;
+  var direction = document.querySelector('#direction').value;
+  var data = url + '?station=' + station + '&direction=' + direction;
 
-	var station = document.querySelector('#station').value;
-	var direction = document.querySelector('#direction').value;
-
-	var data = url + '?station=' + station + '&direction=' + direction;
-
-	//Create a new AJAX request object
-	var request = new XMLHttpRequest();
-	//Open conection to server
-	request.open('GET', data);
-	//Run our handleResponse function when the server responds
-	request.addEventListener('readystatechange', handleResponse);
-	//Actually send the request
-	request.send();
+  //Create a new AJAX request object
+  var request = new XMLHttpRequest();
+  //Open conection to server
+  request.open('GET', data);
+  //Run our handleResponse function when the server responds
+  request.addEventListener('readystatechange', handleResponse);
+  //Actually send the request
+  request.send();
 }
 
+//Handle AJAX response function
 function handleResponse(){
-	console.log(this);
-	var request = this;
+  console.log(this);
+  var request = this;
 
-	//Exit function unless server responded
-	if(request.readyState != 4){
-		return;
-	}
+  //Exit function unless server responded
+  if(request.readyState != 4){
+    return;
+  }
 
-	//If there wasn't an error, round showReponse function
-	if(request.status == 200){
-		var ajaxResponse = JSON.parse(request.responseText);
-		showResponse(ajaxResponse);
-	}
+  //If there wasn't an error, round showReponse function
+  if(request.status == 200){
+    var ajaxResponse = JSON.parse(request.responseText);
+    showResponse(ajaxResponse);
+  }
 }
 
+//Container variables
+var responseContainer = document.querySelector('#result');
+var optionsContainer = document.querySelector('#input');
+
+//Display AJAX response
 function showResponse(ajaxResponse){
-	var responseContainer = document.querySelector('#result');
-	responseContainer.innerHTML = "<p>The next train departing from " + ajaxResponse.stopName + " is at " + ajaxResponse.result[0] + ".</p>";
+  responseContainer.innerHTML = "<p>The next train departing from " + ajaxResponse.stopName + " is at " + ajaxResponse.result[0] + 
+  ". The following train is departing at " + ajaxResponse.result[1] + ".</p> <span class='btn'><button id='back-btn' class='btn'><span>Go Back</span></button></span>";
   //show response container
-  responseContainer.setAttribute("style", "display: flex;");
+  optionsContainer.style.display="none";
+  optionsContainer.classList.remove('show');
+  responseContainer.classList.add('show');
 }
 
+//Event listener for app
+var app = document.getElementById("app");
+app.addEventListener("click", function(e){
 
-ajaxBtn();
 
+console.log(e.target.id);
+//AJAX call button 
+  if(e.target.id == "submit-btn"){
+      //submit AJAX call
+      submit();
+  }
+
+  if(e.target.id == "back-btn"){
+      //Go back to form
+        responseContainer.style.display="none";
+        responseContainer.classList.remove('show');
+        optionsContainer.classList.add('show');
+  }
+
+});
 
 //Navbar stuff
 
@@ -70,50 +90,7 @@ navbarToggle.addEventListener('click',function(){
     }
 });
 
+})();
 
-//Scroll to 
 
 
-$(document).ready(function(){
-
-//Init nice select
-$('select').niceSelect();
-
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-  	console.log("working");
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
-});
